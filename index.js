a0e45161e0187beebc053f825b9671116d9a99e5
chrisnatali <chris.natali@gmail.com>
2016-02-23 09:52:48 -0500

    working version 0.0.2

--- index.js
+++ index.js
@@ -1,42 +1,14 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
-var bodyParser = require('body-parser');
-var layers = require('./layers');
-var views = require('./views');
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   console.log('%s %s', req.method, req.path);
   next();  
 });
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.post('/layers', function(req, res) { 
-  var layer = req.body;
-  layers[layer.id] = layer;
-  res.send(layers);
-});
-  
-app.get('/layers', function(req, res) {
-  res.send(layers);
-});
-
-app.post('/views', function(req, res) { 
-  var view = req.body;
-  views[view.id] = view;
-  res.send(views);
-});
-  
-app.get('/views', function(req, res) {
-  res.send(views);
-});
+app.use(express.static('data'));
+app.use(express.static(__dirname));
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
---
+++ map.js
@@ -0,0 +1,66 @@
+map = function() {
+
+  var layers = undefined,
+      views = undefined,
+      leaflet = undefined,
+      map_element = 'map',
+      onMoveEnd = function() { },
+      _map = undefined;
+      
+  function map(view_id) {
+      // get current or default view 
+      var view = views.hasOwnProperty(view_id) ? views[view_id] : views[Object.keys(views)[0]];
+      // handle initial and reload cases
+      if(_map == undefined) {
+        _map = leaflet.map(map_element);
+      } else {  //reload
+        _map.remove();
+        _map = leaflet.map(map_element);
+      }
+      _map.setView([view.center.x, view.center.y], view.zoom);
+      var view_layers = view.layers.map(function(layer_id) { return layers[layer_id]; });
+      console.log(view_layers);
+      // add layers in sorted order
+      for(i = 0; i < view_layers.length; i++) {
+          layer = view_layers[i];
+          leaflet.tileLayer(layer.url, { 
+                            attribution: layer.attribution, 
+                            maxZoom: layer.maxZoom,
+                            id: layer.id,
+                            cache_bust:  function() { return Math.random(); }
+          }).addTo(_map);
+      };
+  };
+
+  map.layers = function(_) {
+    if (!arguments.length) return layers;
+    layers = _;
+    return map;
+  };
+
+  map.views = function(_) {
+    if (!arguments.length) return views;
+    views = _;
+    return map;
+  };
+
+  map.leaflet = function(_) {
+    if (!arguments.length) return leaflet;
+    leaflet = _;
+    return map;
+  };
+
+  map.map_element = function(_) {
+    if (!arguments.length) return map_element;
+    map_element = _;
+    return map;
+  };
+
+  map.onMoveEnd = function(_) {
+    if (!arguments.length) return onMoveEnd;
+    onMoveEnd = _;
+    return map;
+  };
+
+  return map;
+};
